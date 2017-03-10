acedc991117ff2ff5912b448c68bb8ab666da658
fengzero <fengzero526@qq.com>
2017-03-10 21:19:49 +0800

    send packet

--- arp.c
+++ arp.c
@@ -1,34 +1,47 @@
 #include<stdio.h>
+#include<netinet/if_ether.h>
 #include<errno.h>
 #include<string.h>
 #include<arpa/inet.h>
 #include<memory.h>
+#include<netpacket/packet.h>
 #include<sys/socket.h>
 #include"arp.h"
 
 int main(int argc, char** argv)
 {
-  if(argc != 3){
+  if(argc != 2){
     printf("Usage: fake_ip  fak_mac\n");
     return -1;
   }
   
   char* fakeIp;
+  char* desIp;
   char* mac;
 
- // fakeIp = inet_addr(argv[1]);
-  fakeIp = argv[1];
-  mac = argv[2];
+  fakeIp = inet_addr("192.168.0.1");
+  desIp = inet_addr(argv[1]);
+  mac = "7c:04:d0:c5:9b:48"
 
   int s;
-  s = socket(PF_PACKET, SOCK_RAW, htons(0x0003));
+  s = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
   if(s<0){
     printf("socket raw error: %s\n",strerror(errno));
     return -1;
   }
-  printf("socket ok\n");
-  printf("send arp packet: ip: %s, mac: %s", fakeIp, mac);
-  
+
+  struct sockaddr_ll saddr_ll;
+  saddr_ll.sll_ifindex = IF_PORT_100BASET;
+  saddr_ll.sll_family = PF_PACKET;
+
+  int ret;
+  char* pack;
+  pack = BuildArpPacket(mac, fakeIp, desIp); 
+  ret = sendto(s, pack, sizeof(*pack), 0, (struct sockaddr*)&saddr_ll, sizeof(struct sockaddr_ll));
+  if(ret > 0){
+    printf("send ok\n");
+  }
+  close(s);
   return 0;
 }
 
